3de83b2a7cd0f7c3fabfc5232aa99d56d3ce6013
bradleybiav <bradley@braininavat.dance>
2025-07-15 18:12:10 -0400

    feat: add API key auth middleware

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 import express from 'express';
+
+app.use((req, res, next) => {
+  const apiKey = req.headers["x-api-key"];
+  if (apiKey !== process.env.MCP_API_KEY) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+  next();
+});
 import dotenv from 'dotenv';
 import Airtable from 'airtable';
 
